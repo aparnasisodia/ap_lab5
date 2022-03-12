4569f55ebf2dba1ba92b391ea975f416a50009fd
Owner <owner@Aparnas-MBP.lan>
2022-03-12 12:31:44 -0800

    aos

---
+++ aos_tests_lab4.py
@@ -0,0 +1,16 @@
+import unittest
+import aos_locators as locators
+import aos_method as method
+
+class aosAppPostiveTestCases(unittest.TestCase):
+
+    @staticmethod   # single to units test that this is a static method
+    def test_create_new_user():
+        method.setup()
+        method.create_new_user()
+        method.log_out()
+        method.log_in()
+        method.homepage_texts()
+        method.top_menu()
+        method.contact_us_form()
+        method.tearDown()
