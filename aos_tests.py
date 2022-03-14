4a3c607f5f3db916f3d7144e900eca409ed5c6a9
Owner <owner@Aparnas-MBP.lan>
2022-03-14 16:46:50 -0700

    aos

--- aos_tests.py
+++ aos_tests.py
@@ -5,9 +5,12 @@ import aos_method as method
 class aosAppPostiveTestCases(unittest.TestCase):
 
     @staticmethod   # single to units test that this is a static method
-    def test_create_new_user():
-        method.setUp()
+    def test_create():
+        method.setup()
         method.create_new_user()
         method.log_out()
         method.log_in()
+        method.homepage_texts()
+        method.top_menu()
+        method.contact_us_form()
         method.tearDown()
